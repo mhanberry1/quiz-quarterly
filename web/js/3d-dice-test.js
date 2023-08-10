7066ec950ea2b93bdc04a550732d58577c0fa59c
jackle1127 <jack@foundry45.com>
2023-08-10 11:08:07 -0700

    Updated test.

--- web/js/3d-dice-test.js
+++ web/js/3d-dice-test.js
@@ -3,8 +3,8 @@ import { FBXLoader } from 'three/addons/loaders/FBXLoader'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const canvas = document.querySelector('.webgl');
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+renderer.setSize(600, 600);
 
 const scene = new THREE.Scene();
 // scene.background = new THREE.Color("rgb(255, 0, 0)");
@@ -12,21 +12,21 @@ const scene = new THREE.Scene();
 // Lighting
 var light = new THREE.PointLight();
 light.position.set(0,1,2);
-scene.add(light);
+// scene.add(light);
 light = new THREE.PointLight();
 light.position.set(1,1,-2);
-scene.add(light);
+// scene.add(light);
 const ambientLight = new THREE.AmbientLight(0x888888);
-scene.add(ambientLight);
+// scene.add(ambientLight);
 
-const testGeometry = new THREE.SphereGeometry(1,45,45);
-const testMat = new THREE.MeshStandardMaterial({color: "#06ff22"});
-const testMesh = new THREE.Mesh(testGeometry, testMat);
+// const testGeometry = new THREE.SphereGeometry(1,45,45);
+// const testMat = new THREE.MeshStandardMaterial({color: "#06ff22"});
+// const testMesh = new THREE.Mesh(testGeometry, testMat);
 // scene.add(testMesh);
-console.log(testMesh);
+// console.log(testMesh);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, 800/600, .1, 30);
+const camera = new THREE.PerspectiveCamera(40, 600/600, .1, 30);
 camera.position.set(0, .1, 0);
 scene.add(camera);
 
@@ -48,14 +48,16 @@ fbxLoader.load(
 	}
 );
 */
+
 const gltfLoader = new GLTFLoader();
 gltfLoader.load(
+	// "assets/environment/environment.gltf",
 	"assets/dice-set/scene.gltf",
 	(gltf) => {
 		console.log(gltf);
 		scene.add(gltf.scene);
 		gltf.scene.scale.set(.012, .012, .012);
-		renderer.render(scene, camera);
+		// renderer.render(scene, camera);
 	},
 	(xhr) => {
 			console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -65,17 +67,35 @@ gltfLoader.load(
 	}
 );
 
+gltfLoader.load(
+	"assets/environment/environment.gltf",
+	(gltf) => {
+		console.log(gltf);
+		scene.add(gltf.scene);
+		// gltf.scene.scale.set(.012, .012, .012);
+		// renderer.render(scene, camera);
+		console.log(scene);
+	},
+	(xhr) => {
+			console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+	},
+	(error) => {
+			console.log(error);
+	}
+);
+
+
 var test = 0;
 setInterval(
 	() => {
-		test += .01;
+		test += .03;
 		camera.position.z = Math.sin(test) * .1;
 		camera.position.x = Math.cos(test) * .1;
 		camera.lookAt(0, 0, 0);
 		renderer.render(scene, camera);
 		// console.log(camera.rotation);
 	},
-	1/60
+	1000/60
 );
 
 renderer.render(scene, camera);
